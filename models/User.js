3571538708a8851afba4d7a406fd59577483ec65
rochelledavis <96760168+rochelledavis@users.noreply.github.com>
2022-05-21 10:55:25 -0600

    User Model created

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mondgoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.reduce(
+    (total, friends) => total + friends.replies.length + 1,
+    0
+  );
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
